1deeb6a3d92683aa14e5bcb6af59207b5cad1901
Ivan Babintsev <alon.sage@gmail.com>
2021-06-30 19:04:32 +0300

    Added examples from lesson

---
+++ ok-m1l3-oop/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(kotlin("test-junit"))
+}
