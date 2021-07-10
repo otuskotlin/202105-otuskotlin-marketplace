36f5d71bc4f3ab358c71a5eb8d4c0874921d1065
Sergey Okatov <sokatov@gmail.com>
2021-07-10 12:21:12 +0500

    M1L7 s7 js interoperability + d.ts files

--- ok-m1l7-kmp/build.gradle.kts
+++ ok-m1l7-kmp/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
     /* Targets configuration omitted. 
     *  To find out how to configure the targets, please follow the link:
     *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-    js {
+    js(IR) {
         browser()
         nodejs()
     }
