8b184d1e33bcbc9e5715f8e4bc9de2d889521e84
Sergey Okatov <sokatov@gmail.com>
2021-07-25 16:05:52 +0500

    M3L1-s2 генерация API для Kotlin: комментарии к коду

--- ok-marketplace-be-transport-openapi/build.gradle.kts
+++ ok-marketplace-be-transport-openapi/build.gradle.kts
@@ -3,29 +3,42 @@ plugins {
     id("org.openapi.generator")
 }
 
+/**
+ * Настраиваем генерацию здесь
+ */
 openApiGenerate {
     val openapiGroup = "${rootProject.group}.openapi"
-    generatorName.set("kotlin")
+    generatorName.set("kotlin") // Это и есть активный генератор
     packageName.set(openapiGroup)
     apiPackage.set("$openapiGroup.api")
     modelPackage.set("$openapiGroup.models")
     invokerPackage.set("$openapiGroup.invoker")
     inputSpec.set("$rootDir/specs/marketplace.api-spec.yaml")
 
+    /**
+     * Здесь указываем, что нам нужны только модели, все остальное не нужно
+     */
     globalProperties.apply {
         put("models", "")
         put("modelDocs", "false")
     }
 
+    /**
+     * Настройка дополнительных параметров из документации по генератору
+     * https://github.com/OpenAPITools/openapi-generator/blob/master/docs/generators/kotlin.md
+     */
     configOptions.set(mapOf(
         "dateLibrary" to "string",
         "enumPropertyNaming" to "UPPERCASE",
-        "library" to "multiplatform",
+        "library" to "multiplatform", // Эта опция не отрабатывает. Никакого KMP не генерируется
         "serializationLibrary" to "jackson",
         "collectionType" to "list"
     ))
 }
 
+/**
+ * Здесь подключаем сгенерированные исходники к списку директорий компилятора, чтоб он их видел при компиляции
+ */
 sourceSets {
     main {
         java.srcDir("$buildDir/generate-resources/main/src/main/kotlin")
@@ -38,13 +51,18 @@ dependencies {
 
     implementation(kotlin("stdlib"))
 
-//    implementation "org.jetbrains.kotlin:kotlin-reflect:$kotlin_version"
+    /**
+     * Зависимости ниже мы забрали из сгенерированного build.gradle. Они нужны для компиляции подпроекта
+     */
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
 
 }
 
 tasks {
+    /**
+     * Устанавливаем зависимость компиляции от генерации исходников. Компиляция начнется только после генерации
+     */
     compileKotlin {
         dependsOn(openApiGenerate)
     }
