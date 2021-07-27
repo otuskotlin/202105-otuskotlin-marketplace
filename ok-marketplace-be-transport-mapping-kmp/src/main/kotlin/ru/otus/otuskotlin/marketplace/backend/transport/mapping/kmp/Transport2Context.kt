b58103dc99438dc00916ba756dcaa904b8767815
DTadescu <tada.mascot@gmail.com>
2021-07-27 23:03:39 +0500

    M3L2 Модуль маппинга запросов и ответов с тестом

---
+++ ok-marketplace-be-transport-mapping-kmp/src/main/kotlin/ru/otus/otuskotlin/marketplace/backend/transport/mapping/kmp/Transport2Context.kt
@@ -0,0 +1,62 @@
+package ru.otus.otuskotlin.marketplace.backend.transport.mapping.kmp
+
+import ru.otus.otuskotlin.marketplace.backend.common.context.MpContext
+import ru.otus.otuskotlin.marketplace.backend.common.models.*
+import ru.otus.otuskotlin.marketplace.kmp.transport.models.*
+
+fun MpContext.setQuery(query: InitAdRequest) = apply {
+    onRequest = query.requestId?:""
+}
+
+fun MpContext.setQuery(query: CreateAdRequest) = apply {
+    onRequest = query.requestId?:""
+    requestAd = query.createAd?.toModel()?: AdModel.NONE
+}
+
+fun MpContext.setQuery(query: ReadAdRequest) = apply {
+    onRequest = query.requestId?:""
+    requestAdId = AdIdModel(query.readAdId?:"")
+}
+
+fun MpContext.setQuery(query: UpdateAdRequest) = apply {
+    onRequest = query.requestId?:""
+    requestAd = query.createAd?.toModel()?: AdModel.NONE
+}
+
+fun MpContext.setQuery(query: DeleteAdRequest) = apply {
+    onRequest = query.requestId?:""
+    requestAdId = AdIdModel(query.deleteAdId?:"")
+}
+
+fun MpContext.setQuery(query: OffersAdRequest) = apply {
+    onRequest = query.requestId?:""
+    requestPage = query.page?.toModel()?: PaginatedModel.NONE
+    requestAdId = AdIdModel(query.deleteAdId?:"")
+}
+
+fun MpContext.setQuery(query: SearchAdRequest) = apply {
+    onRequest = query.requestId?:""
+    requestPage = query.page?.toModel()?: PaginatedModel.NONE
+}
+
+private fun BasePaginatedRequest.toModel() = PaginatedModel(
+    size = size?: Int.MIN_VALUE,
+    lastId = AdIdModel(lastId?:""),
+)
+
+private fun UpdateableAd.toModel() = AdModel(
+    id = AdIdModel(id?:""),
+    title = title?:"",
+    description = description?:"",
+    ownerId = OwnerIdModel(ownerId?:""),
+    visibility = visibility?.let { AdVisibilityModel.valueOf(it.name) }?:AdVisibilityModel.NONE,
+    dealSide = dealSide?.let { DealSideModel.valueOf(it.name) }?:DealSideModel.NONE,
+)
+
+private fun CreateableAd.toModel() = AdModel(
+    title = title?:"",
+    description = description?:"",
+    ownerId = OwnerIdModel(ownerId?:""),
+    visibility = visibility?.let { AdVisibilityModel.valueOf(it.name) }?:AdVisibilityModel.NONE,
+    dealSide = dealSide?.let { DealSideModel.valueOf(it.name) }?:DealSideModel.NONE,
+)
