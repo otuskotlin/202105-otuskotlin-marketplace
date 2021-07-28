c122efa210a668920fc7278d969a9c05d0002f47
DTadescu <tada.mascot@gmail.com>
2021-07-28 23:11:01 +0500

    M3L2 Полиморфный сериализатор

---
+++ ok-marketplace-mp-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/kmp/transport/JsonSerializer.kt
@@ -0,0 +1,29 @@
+package ru.otus.otuskotlin.marketplace.kmp.transport
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import ru.otus.otuskotlin.marketplace.kmp.transport.models.*
+
+val jsonSerializer = Json {
+    prettyPrint = true
+    serializersModule = SerializersModule {
+        polymorphic(BaseMessage::class) {
+            subclass(InitAdRequest::class, InitAdRequest.serializer())
+            subclass(CreateAdRequest::class, CreateAdRequest.serializer())
+            subclass(ReadAdRequest::class, ReadAdRequest.serializer())
+            subclass(UpdateAdRequest::class, UpdateAdRequest.serializer())
+            subclass(DeleteAdRequest::class, DeleteAdRequest.serializer())
+            subclass(OffersAdRequest::class, OffersAdRequest.serializer())
+            subclass(SearchAdRequest::class, SearchAdRequest.serializer())
+            subclass(InitAdResponse::class, InitAdResponse.serializer())
+            subclass(CreateAdResponse::class, CreateAdResponse.serializer())
+            subclass(ReadAdResponse::class, ReadAdResponse.serializer())
+            subclass(UpdateAdResponse::class, UpdateAdResponse.serializer())
+            subclass(DeleteAdResponse::class, DeleteAdResponse.serializer())
+            subclass(OffersAdResponse::class, OffersAdResponse.serializer())
+            subclass(SearchAdResponse::class, SearchAdResponse.serializer())
+        }
+
+    }
+}
