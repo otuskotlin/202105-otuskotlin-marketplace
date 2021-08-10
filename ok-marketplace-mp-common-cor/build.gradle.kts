ff608b7aba133fabca200f78e54ffe31e90a467e
Sergey Okatov <sokatov@gmail.com>
2021-08-10 21:27:53 +0500

    M4L1 Инициализация подпроекта

---
+++ ok-marketplace-mp-common-cor/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    js {
+        browser()
+        nodejs()
+    }
+    jvm {
+
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
