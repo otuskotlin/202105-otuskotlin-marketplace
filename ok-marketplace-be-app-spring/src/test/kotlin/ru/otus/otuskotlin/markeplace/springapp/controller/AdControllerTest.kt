6044540be5b6c46e74116b02eb4bdbff205e0498
Dmitry Terekhov <dmitry.terekhov@leroymerlin.ru>
2021-08-11 17:57:47 +0300

    STEP 4 - minor test

--- ok-marketplace-be-app-spring/src/test/kotlin/ru/otus/otuskotlin/markeplace/springapp/controller/AdControllerTest.kt
+++ ok-marketplace-be-app-spring/src/test/kotlin/ru/otus/otuskotlin/markeplace/springapp/controller/AdControllerTest.kt
@@ -1,9 +1,24 @@
 package ru.otus.otuskotlin.markeplace.springapp.controller
 
+import com.ninjasquad.springmockk.MockkBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import ru.otus.otuskotlin.markeplace.springapp.service.AdService
 
 @WebMvcTest(controllers = [AdController::class])
 internal class AdControllerTest {
 
+    @MockkBean
+    private lateinit var adService: AdService
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    fun test() {
+        mockMvc.perform {
+
+        }
+    }
 
 }
