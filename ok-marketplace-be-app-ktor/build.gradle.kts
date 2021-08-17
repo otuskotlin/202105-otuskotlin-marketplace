1b692a583aae3a5c188455c6d8787f7d397c9593
Kirill <kirill.krylov@jetbrains.com>
2021-08-17 20:47:05 +0300

    m1l4: add ktor function for implementation

--- ok-marketplace-be-app-ktor/build.gradle.kts
+++ ok-marketplace-be-app-ktor/build.gradle.kts
@@ -1,4 +1,8 @@
-val ktor: String by project
+val ktorVersion: String by project
+//val kotlinVersion: String by project
+
+fun DependencyHandler.ktor(module: String, version: String? = ktorVersion): Any =
+    "io.ktor:ktor-$module:$version"
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
@@ -14,13 +18,12 @@ repositories {
 }
 
 dependencies {
-    // ktor dependencies
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("io.ktor:ktor-server-core:$ktor")
-    implementation("io.ktor:ktor-server-netty:$ktor")
+    implementation(ktor("server-core")) // "io.ktor:ktor-server-core:$ktorVersion"
+    implementation(ktor("server-netty")) // "io.ktor:ktor-ktor-server-netty:$ktorVersion"
 
-    // logging
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    // logging if you want
+    implementation("ch.qos.logback:logback-classic:1.2.5")
 
     // transport models
     implementation(project(":ok-marketplace-be-common"))
@@ -28,4 +31,7 @@ dependencies {
     implementation(project(":ok-marketplace-be-transport-mapping-openapi"))
     // stubs
     implementation(project(":ok-marketplace-be-stubs"))
+
+    testImplementation(kotlin("test-junit"))
+    testImplementation(ktor("server-test-host")) // "io.ktor:ktor-server-test-host:$ktorVersion"
 }
