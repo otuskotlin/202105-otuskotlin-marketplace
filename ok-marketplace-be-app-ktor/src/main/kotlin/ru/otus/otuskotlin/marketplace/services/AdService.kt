03d1a5d3d9a376bd9308839363667cc288691653
Kirill <kirill.krylov@jetbrains.com>
2021-08-18 13:44:14 +0300

    m1l4: add services from spring

---
+++ ok-marketplace-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/services/AdService.kt
@@ -0,0 +1,74 @@
+package ru.otus.otuskotlin.marketplace.services
+
+import marketplace.stubs.Bolt
+import ru.otus.otuskotlin.marketplace.backend.common.context.MpContext
+import ru.otus.otuskotlin.marketplace.backend.common.models.IError
+
+class AdService {
+
+    fun createAd(mpContext: MpContext): MpContext {
+        return mpContext.apply {
+            responseAd = Bolt.getModel()
+        }
+    }
+
+    fun readAd(mpContext: MpContext): MpContext {
+        val requestedId = mpContext.requestAdId.id
+        val shouldReturnStub = Bolt.isCorrectId(requestedId)
+
+        return if (shouldReturnStub) {
+            mpContext.apply {
+                responseAd = Bolt.getModel()
+            }
+        } else {
+            mpContext.addError {
+                field = "requestedAdId"
+                message = "Not found ad by id $requestedId"
+            }
+        }
+    }
+
+    fun updateAd(context: MpContext) = context.apply {
+        responseAd = requestAd
+    }
+
+
+    fun deleteAd(context: MpContext): MpContext {
+        val shouldReturnStub = Bolt.isCorrectId(context.requestAdId.id)
+
+        return if (shouldReturnStub) {
+            context.apply {
+                responseAd = requestAd
+            }
+        } else {
+            context.addError {
+                field = "id"
+                level = IError.Level.WARNING
+                message = "Ad with id ${context.requestAd.id.id} doesn't exist"
+            }
+        }
+    }
+
+    fun findAd(context: MpContext): MpContext {
+        val requestPage = context.requestPage
+
+        val idToFind = requestPage.lastId
+
+        val shouldReturnStub = Bolt.isCorrectId(idToFind.id)
+
+        return if (shouldReturnStub) {
+            context.apply {
+                responseAds.addAll(
+                    Bolt.getModels()
+                )
+            }
+        } else {
+            context.addError {
+                field = "id"
+                level = IError.Level.WARNING
+                message = "Ad with id ${idToFind.id} doesn't exist"
+            }
+        }
+    }
+}
+
---
+++ ok-marketplace-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/services/OfferService.kt
@@ -0,0 +1,24 @@
+package ru.otus.otuskotlin.marketplace.services
+
+import marketplace.stubs.Bolt
+import ru.otus.otuskotlin.marketplace.backend.common.context.MpContext
+import ru.otus.otuskotlin.marketplace.backend.common.models.AdIdModel
+
+class OfferService {
+
+    fun readOffers(context: MpContext): MpContext {
+        val lastAdModelIdOnPage = context.requestPage.lastId
+        val shouldReturnSingle = lastAdModelIdOnPage != AdIdModel.NONE
+
+        return if (shouldReturnSingle) {
+            context.apply {
+                responseAds.add(Bolt.getModel())
+            }
+        } else {
+            context.apply {
+                responseAds.addAll(Bolt.getModels())
+                responsePage = requestPage
+            }
+        }
+    }
+}
