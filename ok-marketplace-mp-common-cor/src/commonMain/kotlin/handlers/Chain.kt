8e29ed53759d1699dced909c43544a6b4be7585b
Sergey Okatov <sokatov@gmail.com>
2021-08-28 12:12:08 +0500

    M5L1 s5 рефакторинг

---
+++ ok-marketplace-mp-common-cor/src/commonMain/kotlin/handlers/Chain.kt
@@ -0,0 +1,53 @@
+package ru.otus.otuskotlin.marketplace.common.cor.handlers
+
+import ru.otus.otuskotlin.marketplace.common.cor.ICorChainDsl
+import ru.otus.otuskotlin.marketplace.common.cor.ICorExec
+import ru.otus.otuskotlin.marketplace.common.cor.ICorExecDsl
+import ru.otus.otuskotlin.marketplace.common.cor.ICorWorker
+
+fun <T> ICorChainDsl<T>.chain(function: CorChainDsl<T>.() -> Unit) {
+    add(CorChainDsl<T>().apply(function))
+}
+
+class CorChain<T>(
+    private val execs: List<ICorExec<T>>,
+    override val title: String,
+    override val description: String = "",
+    val blockOn: T.() -> Boolean = { true },
+    val blockExcept: T.(Throwable) -> Unit = {},
+) : ICorWorker<T> {
+    override suspend fun on(context: T): Boolean = blockOn(context)
+    override suspend fun except(context: T, e: Throwable) = blockExcept(context, e)
+
+    override suspend fun handle(context: T) {
+        execs.forEach { it.exec(context) }
+    }
+}
+
+class CorChainDsl<T>(
+    override var title: String = "",
+    override var description: String = "",
+    private val workers: MutableList<ICorExecDsl<T>> = mutableListOf(),
+    private var blockOn: T.() -> Boolean = { true },
+    private var blockExcept: T.(e: Throwable) -> Unit = { e: Throwable -> throw e }
+) : ICorChainDsl<T> {
+    override fun build(): ICorExec<T> = CorChain<T>(
+        title = title,
+        description = description,
+        execs = workers.map { it.build() }.toList(),
+        blockOn = blockOn,
+        blockExcept = blockExcept
+    )
+
+    override fun add(worker: ICorExecDsl<T>) {
+        workers.add(worker)
+    }
+
+    override fun on(function: T.() -> Boolean) {
+        blockOn = function
+    }
+
+    override fun except(function: T.(e: Throwable) -> Unit) {
+        blockExcept = function
+    }
+}
