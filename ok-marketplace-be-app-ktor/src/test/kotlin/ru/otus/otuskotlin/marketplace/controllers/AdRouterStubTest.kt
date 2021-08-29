5fe4e5b2e5c16e30e178c5959ebb28271197abd7
DTadescu <tada.mascot@gmail.com>
2021-08-29 19:07:18 +0500

    M5L2 Интеграция в сервис, приложения, правка тестов

--- ok-marketplace-be-app-ktor/src/test/kotlin/ru/otus/otuskotlin/marketplace/controllers/AdRouterStubTest.kt
+++ ok-marketplace-be-app-ktor/src/test/kotlin/ru/otus/otuskotlin/marketplace/controllers/AdRouterStubTest.kt
@@ -1,20 +1,21 @@
 package ru.otus.otuskotlin.marketplace.controllers
 
-import marketplace.stubs.Bolt
+import ru.otus.otuskotlin.marketplace.stubs.Bolt
 import org.junit.Test
 import ru.otus.otuskotlin.marketplace.Utils
 import ru.otus.otuskotlin.marketplace.Utils.assertListEquals
+import ru.otus.otuskotlin.marketplace.Utils.stubCreatableAd
+import ru.otus.otuskotlin.marketplace.Utils.stubSuccessDebug
 import ru.otus.otuskotlin.marketplace.Utils.stubUpdateableAd
 import ru.otus.otuskotlin.marketplace.openapi.models.*
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
-import kotlin.test.assertTrue
 
 class AdRouterTest : RouterTest() {
     @Test
     fun testPostAdCreate() {
-        val data = CreateAdRequest(debug = Utils.stubDebug)
+        val data = CreateAdRequest(createAd = stubCreatableAd, debug = Utils.stubSuccessDebug)
 
         testPostRequest<CreateAdResponse>(data, "/ad/create") {
             assertEquals(CreateAdResponse.Result.SUCCESS, result)
@@ -25,52 +26,31 @@ class AdRouterTest : RouterTest() {
 
     @Test
     fun testPostAdRead() {
-        val data = ReadAdRequest(readAdId = "99999", debug = Utils.stubDebug)
+        val data = ReadAdRequest(readAdId = "99999", debug = Utils.stubSuccessDebug)
 
         testPostRequest<ReadAdResponse>(data, "/ad/read") {
-            assertEquals(ReadAdResponse.Result.ERROR, result)
-            assertNotNull(errors)
-            errors!!.apply {
-                assertTrue(isNotEmpty())
-                assertNotNull(find {
-                    it.field == "requestedAdId" && (it.message?.contains("99999") ?: false)
-                })
-            }
-        }
-
-        testPostRequest<ReadAdResponse>(data.copy(readAdId = "666"), "/ad/read") {
             assertEquals(ReadAdResponse.Result.SUCCESS, result)
             assertNull(errors)
-            assertEquals(Utils.stubResponseAd, readAd)
+            assertEquals(Utils.stubResponseAd.copy(id = "99999"), readAd)
         }
     }
 
     @Test
     fun testPostAdUpdate() {
-        val data = UpdateAdRequest(createAd = stubUpdateableAd, debug = Utils.stubDebug)
+        val data = UpdateAdRequest(createAd = stubUpdateableAd, debug = Utils.stubSuccessDebug)
 
         testPostRequest<UpdateAdResponse>(data, "/ad/update") {
             assertEquals(UpdateAdResponse.Result.SUCCESS, result)
             assertNull(errors)
-            // permissions must be null according to stubs
-            assertEquals(Utils.stubResponseAd.copy(permissions = null), updatedAd)
+            assertEquals(Utils.stubResponseAd, updatedAd)
         }
     }
 
     @Test
     fun testPostAdDelete() {
-        val data = DeleteAdRequest(deleteAdId = "98765", debug = Utils.stubDebug)
+        val data = DeleteAdRequest(deleteAdId = "98765", debug = Utils.stubSuccessDebug)
 
         testPostRequest<DeleteAdResponse>(data, "/ad/delete") {
-            assertEquals(DeleteAdResponse.Result.SUCCESS, result)
-            assertNotNull(errors)
-            errors!!.apply {
-                assertTrue(isNotEmpty())
-                assertNotNull(find { it.field == "id" })
-            }
-        }
-
-        testPostRequest<DeleteAdResponse>(data.copy(deleteAdId = "666"), "/ad/delete") {
             assertEquals(DeleteAdResponse.Result.SUCCESS, result)
             assertNull(errors)
         }
@@ -78,7 +58,11 @@ class AdRouterTest : RouterTest() {
 
     @Test
     fun testPostAdSearch() {
-        val data = SearchAdRequest(requestId = "99", page = BasePaginatedRequest(size = 3, lastId = "666"))
+        val data = SearchAdRequest(
+            requestId = "99",
+            page = BasePaginatedRequest(size = 3, lastId = "666"),
+            debug = stubSuccessDebug
+        )
 
         testPostRequest<SearchAdResponse>(data, "/ad/search") {
             assertEquals(SearchAdResponse.Result.SUCCESS, result)
