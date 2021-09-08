5adde920a7a98011ba0504fa92d1030dfdb8ae10
DTadescu <tada.mascot@gmail.com>
2021-09-09 00:23:17 +0500

    M6L2 Модуль с зависимостями

--- ok-marketplace-be-app-rabbit/build.gradle.kts
+++ ok-marketplace-be-app-rabbit/build.gradle.kts
@@ -6,12 +6,15 @@ plugins {
 dependencies {
     val rabbitVersion: String by project
     val jacksonVersion: String by project
+    val coroutinesVersion: String by project
 
     implementation(kotlin("stdlib"))
     implementation("com.rabbitmq:amqp-client:$rabbitVersion")
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
     implementation(project(":ok-marketplace-be-service-openapi"))
     implementation(project(":ok-marketplace-be-transport-openapi"))
+    implementation(project(":ok-marketplace-be-transport-mapping-openapi"))
     implementation(project(":ok-marketplace-be-common"))
 }
