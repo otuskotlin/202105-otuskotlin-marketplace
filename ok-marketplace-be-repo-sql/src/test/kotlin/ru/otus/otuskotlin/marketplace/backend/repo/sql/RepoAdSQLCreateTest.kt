f0dd3d65b6e8ba89d6fc89868af8973bf90bc919
Kirill <kirill.krylov@jetbrains.com>
2021-10-05 12:56:18 +0300

    M7L2: add tests for sql

---
+++ ok-marketplace-be-repo-sql/src/test/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/sql/RepoAdSQLCreateTest.kt
@@ -0,0 +1,28 @@
+package ru.otus.otuskotlin.marketplace.backend.repo.sql
+
+import ru.otus.otuskotlin.marketplace.backend.repo.common.IRepoAd
+import ru.otus.otuskotlin.marketplace.backend.repo.test.*
+
+class RepoAdSQLCreateTest : RepoAdCreateTest() {
+    init {
+        println(System.getProperties())
+    }
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
+}
+
+class RepoAdSQLDeleteTest : RepoAdDeleteTest() {
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
+}
+
+class RepoAdSQLReadTest : RepoAdReadTest() {
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
+}
+
+class RepoAdSQLSearchTest : RepoAdSearchTest() {
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
+}
+
+class RepoAdSQLUpdateTest : RepoAdUpdateTest() {
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
+}
+
