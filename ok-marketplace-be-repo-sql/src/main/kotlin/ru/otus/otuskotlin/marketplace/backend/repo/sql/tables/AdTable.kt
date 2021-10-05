9ca6f74424af3c12553d93f3bafbaa4b5d7eef8d
Kirill <kirill.krylov@jetbrains.com>
2021-10-05 12:55:15 +0300

    M7L2: add ad table

---
+++ ok-marketplace-be-repo-sql/src/main/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/sql/tables/AdTable.kt
@@ -0,0 +1,35 @@
+package ru.otus.otuskotlin.marketplace.backend.repo.sql.tables
+
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.statements.InsertStatement
+import ru.otus.otuskotlin.marketplace.backend.common.models.*
+
+object AdTable : Table("Ads") {
+    val id = varchar("id", 128)
+    val title = varchar("title", 128)
+    val description = varchar("description", 512)
+    val ownerId = varchar("ownerId", 128)
+    val visibility = enumeration("visibility", AdVisibilityModel::class)
+    val dealSide = enumeration("dealSide", DealSideModel::class)
+
+    override val primaryKey = PrimaryKey(id)
+
+    fun from(res: InsertStatement<Number>) = AdModel(
+        id = AdIdModel(res[id]),
+        title = res[title],
+        description = res[description],
+        ownerId = OwnerIdModel(res[ownerId]),
+        visibility = res[visibility],
+        dealSide = res[dealSide]
+    )
+
+    fun from(res: ResultRow) = AdModel(
+        id = AdIdModel(res[id]),
+        title = res[title],
+        description = res[description],
+        ownerId = OwnerIdModel(res[ownerId]),
+        visibility = res[visibility],
+        dealSide = res[dealSide]
+    )
+}
