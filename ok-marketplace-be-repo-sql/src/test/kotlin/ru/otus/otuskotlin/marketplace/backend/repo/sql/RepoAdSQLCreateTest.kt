ca8e77ef80f5d08f4139528fe2dc1123946c4c1d
Kirill <kirill.krylov@jetbrains.com>
2021-10-06 18:16:49 +0300

    Revert "M7L2: add postgres test container"
    This reverts commit 9f52c26e0ef7a1588f1a8e508a0b68027dd8987f.

--- ok-marketplace-be-repo-sql/src/test/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/sql/RepoAdSQLCreateTest.kt
+++ ok-marketplace-be-repo-sql/src/test/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/sql/RepoAdSQLCreateTest.kt
@@ -4,21 +4,22 @@ import ru.otus.otuskotlin.marketplace.backend.repo.common.IRepoAd
 import ru.otus.otuskotlin.marketplace.backend.repo.test.*
 
 class RepoAdSQLCreateTest : RepoAdCreateTest() {
-    override val repo: IRepoAd = SqlTestCompanion.repoUnderTestContainer(initObjects)
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
 }
 
 class RepoAdSQLDeleteTest : RepoAdDeleteTest() {
-    override val repo: IRepoAd = SqlTestCompanion.repoUnderTestContainer(initObjects)
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
 }
 
 class RepoAdSQLReadTest : RepoAdReadTest() {
-    override val repo: IRepoAd = SqlTestCompanion.repoUnderTestContainer(initObjects)
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
 }
 
 class RepoAdSQLSearchTest : RepoAdSearchTest() {
-    override val repo: IRepoAd = SqlTestCompanion.repoUnderTestContainer(initObjects)
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
 }
 
 class RepoAdSQLUpdateTest : RepoAdUpdateTest() {
-    override val repo: IRepoAd = SqlTestCompanion.repoUnderTestContainer(initObjects)
+    override val repo: IRepoAd = RepoAdSQL(initObjects = initObjects)
 }
+
