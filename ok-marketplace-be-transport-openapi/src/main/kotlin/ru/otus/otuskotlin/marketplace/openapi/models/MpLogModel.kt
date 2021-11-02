69cd241c0191a8cfbef53aa6d32a4c27c7d73e42
BarracudaPff <mrm.kikll@gmail.com>
2021-11-02 11:45:56 +0300

    M9L2: rewrite models with openapi

--- ok-marketplace-be-transport-openapi/src/main/kotlin/ru/otus/otuskotlin/marketplace/openapi/models/MpLogModel.kt
+++
@@ -1,8 +0,0 @@
-package ru.otus.otuskotlin.marketplace.openapi.models
-
-data class MpLogModel(
-    val requestAdId: String? = null,
-    val requestAd: ResponseAd? = null,
-    val responseAd: ResponseAd? = null,
-    val responseAds: List<ResponseAd>? = null,
-)
