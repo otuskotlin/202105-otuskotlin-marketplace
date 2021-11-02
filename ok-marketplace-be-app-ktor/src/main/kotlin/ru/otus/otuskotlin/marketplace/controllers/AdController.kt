362d04f723c30edf749fec38b364a2bd4224f277
BarracudaPff <mrm.kikll@gmail.com>
2021-11-02 11:44:28 +0300

    M9L2: optimise imports

--- ok-marketplace-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/controllers/AdController.kt
+++ ok-marketplace-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/controllers/AdController.kt
@@ -1,19 +1,10 @@
 package ru.otus.otuskotlin.marketplace.controllers
 
 import io.ktor.application.*
-import io.ktor.auth.*
-import io.ktor.auth.jwt.*
-import io.ktor.request.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import ru.otus.otuskotlin.marketplace.backend.common.context.MpContext
 import ru.otus.otuskotlin.marketplace.backend.services.AdService
 import ru.otus.otuskotlin.marketplace.backend.transport.mapping.kmp.toCreateResponse
-import ru.otus.otuskotlin.marketplace.logging.MpLogContext
 import ru.otus.otuskotlin.marketplace.logging.mpLogger
-import ru.otus.otuskotlin.marketplace.mappers.toModel
 import ru.otus.otuskotlin.marketplace.openapi.models.*
-import java.time.Instant
 
 suspend fun ApplicationCall.initAd(adService: AdService) = handleRequest<InitAdRequest, InitAdResponse>(
     "init-ad",
