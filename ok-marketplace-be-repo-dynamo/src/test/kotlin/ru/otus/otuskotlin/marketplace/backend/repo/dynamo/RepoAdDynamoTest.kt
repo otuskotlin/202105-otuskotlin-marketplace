842107ed6361a0d84a7c10fe1ed99403169fa759
Sergey Okatov <sokatov@gmail.com>
2021-11-04 10:19:12 +0500

    M8M3-tiny fixes, docker-compose-keycloak.yaml

--- ok-marketplace-be-repo-dynamo/src/test/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/dynamo/RepoAdDynamoTest.kt
+++ ok-marketplace-be-repo-dynamo/src/test/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/dynamo/RepoAdDynamoTest.kt
@@ -3,23 +3,36 @@ package ru.otus.otuskotlin.marketplace.backend.repo.dynamo
 import ru.otus.otuskotlin.marketplace.backend.repo.test.*
 import java.util.*
 
-class RepoAdDynamoCreateTest: RepoAdCreateTest() {
-    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
-}
+/*
+Никогда так не делайте как в этом тесте.
 
-class RepoAdDynamoReadTest: RepoAdReadTest() {
-    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
-}
+Здесь используется боевая база данных dynamoDB с тестовыми таблицами.
 
-class RepoAdDynamoUpdateTest: RepoAdUpdateTest() {
-    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
-}
+В итоге, таблицы создаются после каждой сборки. Если их не подчищать, может набежать довольно крупная сумма за
+использование dynamoDB в AWS.
 
-class RepoAdDynamoDeleteTest: RepoAdDeleteTest() {
-    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
-}
+Тестирование необходимо выполнять с помощью тестового Docker-образа dynamoDb:
+https://hub.docker.com/r/amazon/dynamodb-local
 
-// Не проходит с глобальными индексами, индекс не успевает обновиться
-class RepoAdDynamoSearchTest: RepoAdSearchTest() {
-    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
-}
+ */
+
+//class RepoAdDynamoCreateTest: RepoAdCreateTest() {
+//    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
+//}
+//
+//class RepoAdDynamoReadTest: RepoAdReadTest() {
+//    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
+//}
+//
+//class RepoAdDynamoUpdateTest: RepoAdUpdateTest() {
+//    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
+//}
+//
+//class RepoAdDynamoDeleteTest: RepoAdDeleteTest() {
+//    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
+//}
+//
+//// Не проходит с глобальными индексами, индекс не успевает обновиться
+//class RepoAdDynamoSearchTest: RepoAdSearchTest() {
+//    override val repo = RepoAdDynamo(initObjects = initObjects, table = "test-${UUID.randomUUID()}")
+//}
